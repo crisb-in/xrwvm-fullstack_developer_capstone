0de7c48c60dfec0c389a8f56b763dce39351c8e2
crisb-in <crisbinvignesh25@gmail.com>
2024-09-05 05:24:48 -0400

    Added Admin page

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,22 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
-
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1
 # CarModelAdmin class
-
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country', 'establishment_year')
+    inline = (CarModelInline)
 # CarMakeAdmin class with CarModelInline
-
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')
+    list_filter = ['type', 'year']
+    search_fields = ['name']
 # Register models here
+admin.site.register(CarMake)
+admin.site.register(CarModel)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,13 +15,24 @@ import logging
 from django.contrib.auth import logout
 import json
 from django.views.decorators.csrf import csrf_exempt
+from .models import CarMake, CarModel
 # from .populate import initiate
 
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
+    
 # Create your views here.
 
 # Create a `login_request` view to handle sign in request
