558ecf7e76409e436d083f1399f3773e2528888d
crisb-in <crisbinvignesh25@gmail.com>
2024-09-07 12:13:17 +0530

    models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,4 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -13,42 +10,52 @@ class CarMake(models.Model):
     # Other fields as needed
     country = models.CharField(max_length=100, null=True, blank=True)
     establishment_year = models.IntegerField(null=True, blank=True)
-    logo = models.ImageField(upload_to='car_make_logos/', null=True,blank=True)
+    logo = models.ImageField(upload_to='car_make_logos/', null=True, blank=True)
     website = models.URLField(max_length=200, null=True, blank=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name  # Return the name as the string representation
 
+
 # Car Model Model
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
+
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
-        ('HATCHBACK', 'Hachback'),
+        ('HATCHBACK', 'Hatchback'),
         ('CROSSOVER', 'Crossover'),
         ('SPORTSCAR', 'Sportscar'),
-        
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-        ])
+        ]
+    )
     # Other fields as needed
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    transmission = models.CharField(max_length=10, choices=[('AUTO', 'Automatic'), ('MANUAL', 'Manual')], default='AUTO')
-    fuel_type = models.CharField(max_length=10, choices=[('GAS', 'Gasoline'), ('ELECTRIC', 'Electric')], default='GAS')
+    transmission = models.CharField(
+        max_length=10,
+        choices=[('AUTO', 'Automatic'), ('MANUAL', 'Manual')],
+        default='AUTO'
+    )
+    fuel_type = models.CharField(
+        max_length=10,
+        choices=[('GAS', 'Gasoline'), ('ELECTRIC', 'Electric')],
+        default='GAS'
+    )
     mileage = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     horsepower = models.IntegerField(null=True, blank=True)
     color_options = models.CharField(max_length=100, null=True, blank=True)
     is_available = models.BooleanField(default=True)
 
-
     def __str__(self):
-        return f"{self.name} ({self.car_make.name})" # Return the name as the string representation
+        return f"{self.name} ({self.car_make.name})"  # Return the name as the string representation
